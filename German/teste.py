1e880b6691d8b1de3b684a52a6a66879a8f3d217
Gabriel-Cavalcanti <programacaogabrielcavalcanti18@gmail.com>
2022-07-18 21:39:30 -0300

    t

--- German/teste.py
+++ German/teste.py
@@ -1,6 +1,50 @@
-def x(val):
-    val[0] = 45
+lista_num =[]
+lista_num_impar = []
+lista_num_b = []
+lista_num_c = []
+lista_num_final = []
 
-val = [2,4,6]
+for i in range(100,1000):
+    lista_num.append(i)
 
-print(x(val))
+#print(lista_num)
+
+for k in lista_num:
+
+    if k % 2 != 0:
+        lista_num_impar.append(k)
+        continue
+    else:
+        continue
+
+#print(lista_num_impar)
+
+for l in lista_num_impar:
+
+    if l % 5 == 3:
+        lista_num_b.append(l)
+        continue
+    else:
+        continue
+
+print(lista_num_b)
+
+for n in lista_num_b:
+
+    if n % 7 == 5:
+        lista_num_c.append(n)
+        continue
+    else:
+        continue
+
+print(lista_num_c)
+
+for x in lista_num_c:
+
+    if x % 9 == 1:
+        lista_num_final.append(x)
+        continue
+    else:
+        continue
+
+print(lista_num_final)
--- projeto_yt_Tkinter/main.py
+++ projeto_yt_Tkinter/main.py
@@ -1,14 +1,114 @@
 from tkinter import *
+from tkinter import ttk
+import sqlite3
 
 root = Tk()
 
-class Application:
+class funcs:
+
+    def variaveis(self):
+
+        self.codigo = self.codigo_entry.get()
+        self.nome = self.nome_entry.get()
+        self.telefone = self.telefone_entry.get()
+        self.cidade = self.cidade_entry.get()
+
+
+    def limpar_tela(self):
+        self.codigo_entry.delete(0, END)
+        self.nome_entry.delete(0, END)
+        self.telefone_entry.delete(0, END)
+        self.cidade_entry.delete(0, END)
+    
+    def conecta_bd(self):
+        self.conn = sqlite3.connect("clientes.bd")
+        self.cursor = self.conn.cursor()
+        print("Conectando ao banco de dados")
+    
+    def desconecta_bd(self):
+        self.conn.close()
+        print("desconectando ao banco de dados")
+
+    def monta_tabela(self):
+        self.conecta_bd()
+        # Criando a tabela
+
+        self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS clientes (
+                cod INTERGER PRIMARY KEY,
+                nome_cliente CHAR(40) NOT NULL,
+                telefone INTEGER(20),
+                cidade CHAR(40) 
+            );
+        """)
+        self.conn.commit()
+        print("Banco de dados criado")
+        self.desconecta_bd()
+
+    def add_cliente(self):
+        
+        self.variaveis()
+
+        self.conecta_bd()
+
+        self.cursor.execute(""" INSERT INTO clientes (cod, nome_cliente, telefone, cidade)
+                    VALUES (?, ?, ?, ? ) """, (self.codigo, self.nome, self.telefone, self.cidade)) 
+        
+        self.conn.commit()
+        self.desconecta_bd()
+        self.select_list()
+        self.limpar_tela()
+
+    def select_list(self):
+
+        self.listaCLI.delete(*self.listaCLI.get_children())
+        self.conecta_bd()
+        lista = self.cursor.execute(""" SELECT cod, nome_cliente, telefone, cidade FROM clientes ORDER BY nome_cliente ASC; """)
+
+        for i in lista:
+            self.listaCLI.insert("", END, values=i)
+        
+        self.desconecta_bd()
+    
+    def on_double_click(self, event):
+
+        self.limpar_tela()
+        self.listaCLI.selection()
+
+        for n in self.listaCLI.selection():
+
+            col1, col2, col3, col4 = self.listaCLI.item(n, 'values')
+
+            self.codigo_entry.insert(END, col1)
+            self.nome_entry.insert(END, col2)
+            self.telefone_entry.insert(END, col3)
+            self.cidade_entry.insert(END, col4)
+    
+    def deleta_cliente(self):
+
+        self.variaveis()
+
+        self.conecta_bd()
+
+        self.cursor.execute(""" DELETE FROM clientes WHERE cod = ? """, (self.codigo))
+        self.conn.commit()
+
+        self.desconecta_bd()
+
+        self.limpar_tela()
+        self.select_list()
+
+
+class Application(funcs):
 
     def __init__(self):
         self.root = root
         self.tela()
         self.frame_da_tela()
         self.widget_frame_01()
+        self.lista_frame_02()
+        self.monta_tabela()
+        self.select_list()
         root.mainloop()
     
     def tela(self):
@@ -29,13 +129,14 @@ class Application:
         self.frame_02.place(relx=0.02 , rely=0.5, relwidth=0.96, relheight=0.46)
 
     def widget_frame_01(self):
+    
         #Botões
 
-        self.bt_limpar = Button(self.frame_01, text="Limpar")
-        self.bt_buscar = Button(self.frame_01, text="buscar")
-        self.bt_novo = Button(self.frame_01, text="Novo")
-        self.bt_alterar = Button(self.frame_01, text="Alterar")
-        self.bt_apagar = Button(self.frame_01, text="Apagar")
+        self.bt_limpar = Button(self.frame_01, text="Limpar", bd=2, bg="#107db2", fg="white", font=("verdana", 8, "bold"), command=self.limpar_tela)
+        self.bt_buscar = Button(self.frame_01, text="buscar", bd=2, bg="#107db2", fg="white", font=("verdana", 8, "bold"))
+        self.bt_novo = Button(self.frame_01, text="Novo", bd=2, bg="#107db2", fg="white", font=("verdana", 8, "bold"), command=self.add_cliente)
+        self.bt_alterar = Button(self.frame_01, text="Alterar", bd=2, bg="#107db2", fg="white", font=("verdana", 8, "bold"))
+        self.bt_apagar = Button(self.frame_01, text="Apagar", bd=2, bg="#107db2", fg="white", font=("verdana", 8, "bold"), command=self.deleta_cliente)
 
         self.bt_limpar.place(relx=0.2 , rely=0.1, relwidth=0.1, relheight=0.15)
         self.bt_buscar.place(relx=0.3 , rely=0.1, relwidth=0.1, relheight=0.15)
@@ -45,10 +146,10 @@ class Application:
 
         #Labels
 
-        self.lb_codigo = Label(self.frame_01, text="Código")
-        self.lb_nome = Label(self.frame_01, text="Nome")
-        self.lb_telefone = Label(self.frame_01, text="telefone")
-        self.lb_cidade= Label(self.frame_01, text="cidade")
+        self.lb_codigo = Label(self.frame_01, text="Código", bg="#dfe3ee", fg="#107db2")
+        self.lb_nome = Label(self.frame_01, text="Nome", bg="#dfe3ee", fg="#107db2")
+        self.lb_telefone = Label(self.frame_01, text="telefone", bg="#dfe3ee", fg="#107db2")
+        self.lb_cidade= Label(self.frame_01, text="cidade", bg="#dfe3ee", fg="#107db2")
 
         self.lb_codigo.place(relx=0.05 , rely=0.05)
         self.lb_nome.place(relx=0.05 , rely=0.35)
@@ -66,6 +167,29 @@ class Application:
         self.nome_entry.place(relx=0.05, rely=0.45, relwidth=0.8)
         self.telefone_entry.place(relx=0.05, rely=0.7, relwidth=0.4)
         self.cidade_entry.place(relx=0.5, rely=0.7, relwidth=0.4)
+
+    def lista_frame_02(self):
+        self.listaCLI = ttk.Treeview(self.frame_02, height=3, columns=("col1", "col2", "col3", "col4"))
+
+        self.listaCLI.heading("#0", text=" ")
+        self.listaCLI.heading("#1", text="Código")
+        self.listaCLI.heading("#2", text="Nome")
+        self.listaCLI.heading("#3", text="Telefone")
+        self.listaCLI.heading("#4", text="Cidade")
+
+        self.listaCLI.column("#0", width=1)
+        self.listaCLI.column("#1", width=50)
+        self.listaCLI.column("#2", width=200)
+        self.listaCLI.column("#3", width=125)
+        self.listaCLI.column("#4", width=125)
+
+        self.scroolista = Scrollbar(self.frame_02, orient="vertical")
+        self.listaCLI.configure(yscroll=self.scroolista.set)
+
+        self.listaCLI.place(relx=0.01 , rely=0.1, relwidth=0.95, relheight=0.85)
+        self.scroolista.place(relx=0.96 , rely=0.1, relwidth=0.04, relheight=0.85)
+
+        self.listaCLI.bind("<Double-1>", self.on_double_click)
 
 
     
